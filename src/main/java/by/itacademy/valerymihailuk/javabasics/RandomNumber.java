351d56c4ca1bb6be0c94697de4c2db7c01c88ee3
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-28 23:25:30 +0300

    add random number class

---
+++ src/main/java/by/itacademy/valerymihailuk/javabasics/RandomNumber.java
@@ -0,0 +1,5 @@
+package by.itacademy.valerymihailuk.javabasics;
+
+public class RandomNumber {
+
+}
---
+++ src/main/java/by/itacademy/valerymihailuk/javabasics/Util.java
@@ -0,0 +1,7 @@
+package by.itacademy.valerymihailuk.javabasics;
+
+public class Util {
+    public static final input (){
+
+    }
+}
