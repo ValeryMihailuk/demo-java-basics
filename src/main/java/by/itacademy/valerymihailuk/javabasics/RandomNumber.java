ac885b84b7a9e7fb68cacc937cdfe42560ff1442
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-03-01 10:42:52 +0300

    refactor random number class

--- src/main/java/by/itacademy/valerymihailuk/javabasics/RandomNumber.java
+++ src/main/java/by/itacademy/valerymihailuk/javabasics/RandomNumber.java
@@ -1,5 +1,19 @@
 package by.itacademy.valerymihailuk.javabasics;
 
-public class RandomNumber {
+import java.util.Scanner;
+import java.util.Random;
 
+public class RandomNumber {
+    public static void main(String[] args) {
+        int x;
+        int inputNumber;
+        System.out.println("Введите целое число  в диапозоне от 1 до 10");
+        Scanner input = new Scanner(System.in);
+        inputNumber = input.nextInt();
+        x = (int) Math.pow(10, inputNumber);
+        Random generator = new Random();
+        long randomNumber = generator.nextInt(x);
+        System.out.println(randomNumber);
+
+    }
 }
